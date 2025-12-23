ef317bfe4f2516997404bc54d7ace391c71bdca6
irantzu01 <irantzuelcoro@gmail.com>
2025-12-23 09:47:35 +0100

    load models after deleting cache

--- scripts/latxa_setup.py
+++ scripts/latxa_setup.py
@@ -1,4 +1,12 @@
-from transformers import pipeline
-pipe = pipeline("text-generation", model="HiTZ/latxa-7b-v1.1")
-text = "Euskara adimen artifizialera iritsi da!"
-pipe(text, max_new_tokens=50, num_beams=5)
+# from transformers import pipeline
+# pipe = pipeline("text-generation", model="HiTZ/latxa-7b-v1.1")
+# text = "Euskara adimen artifizialera iritsi da!"
+# pipe(text, max_new_tokens=50, num_beams=5)
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+latxa7B_tokenizer = AutoTokenizer.from_pretrained("HiTZ/latxa-7b-v1.2")
+latxa7B_model = AutoModelForCausalLM.from_pretrained("HiTZ/latxa-7b-v1.2")
+latxa70B_tokenizer = AutoTokenizer.from_pretrained("HiTZ/latxa-70b-v1.2")
+latxa70B_model = AutoModelForCausalLM.from_pretrained("HiTZ/latxa-70b-v1.2")
+llama2_tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+llama2_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
