bf9de10d7b8a83e7e4b60f5e8b1bb95e70a5a2b9
irantzu01 <irantzuelcoro@gmail.com>
2025-12-23 10:33:08 +0100

    load only tokenizers, not models, they are too heavy

--- scripts/latxa_setup.py
+++ scripts/latxa_setup.py
@@ -5,8 +5,8 @@
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 latxa7B_tokenizer = AutoTokenizer.from_pretrained("HiTZ/latxa-7b-v1.2")
-latxa7B_model = AutoModelForCausalLM.from_pretrained("HiTZ/latxa-7b-v1.2")
+#latxa7B_model = AutoModelForCausalLM.from_pretrained("HiTZ/latxa-7b-v1.2")
 latxa70B_tokenizer = AutoTokenizer.from_pretrained("HiTZ/latxa-70b-v1.2")
-latxa70B_model = AutoModelForCausalLM.from_pretrained("HiTZ/latxa-70b-v1.2")
+#latxa70B_model = AutoModelForCausalLM.from_pretrained("HiTZ/latxa-70b-v1.2")
 llama2_tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
-llama2_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+#llama2_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
