80779e6624495f9a27a38469d7ea75a2f3f37a72
irantzu01 <irantzuelcoro@gmail.com>
2026-01-08 12:56:54 +0100

    add sh for corpus creation

--- cluster/run_tokenizer.sh
+++ cluster/run_tokenizer.sh
@@ -30,6 +30,6 @@ cd ~/MASTER/WiSe25/Lab\ Rotation/latxa_tokenizer_eval
 echo "Running python experiment..."
 
 # === 4. RUN YOUR PYTHON SCRIPT ===
-python scripts/eusproficiency_instance_level_evaluation.py
+python scripts/corpus_preparation.py
 
 echo "Job finished."
