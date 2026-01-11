d90c6416ab7a125635ceb628c32d72eb2918b922
irantzu01 <irantzuelcoro@gmail.com>
2026-01-11 20:32:19 +0100

    evaluate new model

--- evaluation/new_model_evaluation.py
+++ evaluation/new_model_evaluation.py
@@ -27,8 +27,8 @@ from tqdm import tqdm
 
 # ==================== CONFIGURATION ====================
 # Change these paths to evaluate different models
-MODEL_PATH = os.path.expanduser("~/tmp/models/latxa7b_basque_aligned_100k_improved/final")
-MODEL_NAME = "100k_improved"  # Used in output filenames
+MODEL_PATH = os.path.expanduser("~/tmp/models/latxa7b_basque_aligned_250k/final")
+MODEL_NAME = "250k"  # Used in output filenames
 
 # Alternative models to test:
 # MODEL_PATH = os.path.expanduser("~/tmp/models/latxa7b_basque_aligned_100k_improved/checkpoint-epoch2")
