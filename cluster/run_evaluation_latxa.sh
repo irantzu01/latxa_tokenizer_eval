9c1880f7245364f1e8678a9b820f3ce109b3e6e7
irantzu01 <irantzuelcoro@gmail.com>
2026-01-11 12:40:20 +0100

    new model evaluation

--- cluster/run_evaluation_latxa.sh
+++ cluster/run_evaluation_latxa.sh
@@ -29,6 +29,6 @@ cd ~/MASTER/WiSe25/Lab\ Rotation/latxa_tokenizer_eval
 echo "Running python experiment..."
 
 # === 4. RUN YOUR PYTHON SCRIPT ===
-python scripts/new_model_evaluation.py
+python evaluation/new_model_evaluation.py
 
 echo "Job finished."
