dcd2afc638cf5f671953e60158faa0c74a8e4643
irantzu01 <irantzuelcoro@gmail.com>
2026-01-12 11:11:42 +0100

    run evaluation with dynamic augmenter

--- cluster/run_evaluation_dyn.sh
+++ cluster/run_evaluation_dyn.sh
@@ -29,6 +29,6 @@ cd ~/MASTER/WiSe25/Lab\ Rotation/latxa_tokenizer_eval
 echo "Running python experiment..."
 
 # === 4. RUN YOUR PYTHON SCRIPT ===
-python evaluation/evaluation_eus_proficiency_with_dynamic_augmenter.py
+python scripts/evaluation_eus_proficiency_with_dynamic_augmenter.py
 
 echo "Job finished."
