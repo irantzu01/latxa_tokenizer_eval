52903265751c614c65dd393b95b73848767ab493
irantzu01 <irantzuelcoro@gmail.com>
2026-01-12 21:19:37 +0100

    evaluation of new model

--- cluster/run_eval_eus_reading.sh
+++ cluster/run_eval_eus_reading.sh
@@ -30,6 +30,6 @@ cd ~/MASTER/WiSe25/Lab\ Rotation/latxa_tokenizer_eval
 echo "Running python experiment..."
 
 # === 4. RUN YOUR PYTHON SCRIPT ===
-python scripts/evaluation_eus_reading.py
+python evaluation/evaluation_eusreading_new_model.py
 
 echo "Job finished."
