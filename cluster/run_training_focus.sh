62cfbd230dfe557cfc47dc18b4c4a8de3c6a786c
irantzu01 <irantzuelcoro@gmail.com>
2026-01-19 22:10:40 +0100

    run training with focus

--- cluster/run_training_focus.sh
+++ cluster/run_training_focus.sh
@@ -30,6 +30,6 @@ cd ~/MASTER/WiSe25/Lab\ Rotation/latxa_tokenizer_eval
 echo "Running python experiment..."
 
 # === 4. RUN YOUR PYTHON SCRIPT ===
-python scripts/train_and_realign_big.py
+python scripts/train_and_realign_focus.py
 
 echo "Job finished."
